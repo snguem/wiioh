b01d6620bcc5ca4065bf8ddc82f60269a613c118
snguem <snguemaabessolo@gmail.com>
2024-09-02 06:32:26 +0000

    first commit

---
+++ src/main/java/com/snode/wiioh/WiiohApplication.java
@@ -0,0 +1,13 @@
+package com.snode.wiioh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiiohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiiohApplication.class, args);
+	}
+
+}
